03f5b08551358d84d11e0688732cdaec7126cbc5
Felipe Reyes <felipe.reyes@canonical.com>
2023-10-11 17:03:31 -0300

    magnum: configure vm before starting it
    This avoids having to stop/start to attach the shared directory.

--- magnum/launch-vm.sh
+++ magnum/launch-vm.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -eux
 
-lxc launch --vm -c limits.cpu=4 -c limits.memory=32GiB j devstack
-until lxc exec devstack -- sh -c "cloud-init status --wait"; do sleep 1;done
-lxc stop devstack
-lxc config device add devstack projectsdir disk source=$HOME/Projects path=/home/ubuntu/Projects
-# lxc config device add devstack eth1 nic name=eth1 nictype=bridged parent=lxdbr0
+
+lxc init --vm j devstack
+lxc config set devstack limits.cpu 4
+lxc config set devstack limits.memory 32GiB
+lxc config device override devstack root size=60GiB
+lxc config device add devstack projectsdir disk source=$HOME/Projects path=/mnt/Projects
 lxc start devstack
-until ssh devstack.lxd test -e /home/ubuntu; do sleep 1;done
+
+until lxc exec devstack -- sh -c "cloud-init status --wait"; do sleep 1;done
+
 ssh devstack.lxd "sudo mkdir /opt/stack; \
     sudo chown ubuntu: /opt/stack; \
     git clone https://opendev.org/openstack/devstack /home/ubuntu/devstack; \
-    ln -s /home/ubuntu/Projects/openstack/mydevstack/magnum/local.conf /home/ubuntu/devstack/"
+    ln -s /mnt/Projects/openstack/mydevstack/magnum/local.conf /home/ubuntu/devstack/ ; \
+    byobu-enable"
+RESOLVE_CONF_FILE=$(mktemp)
+cat <<EOF > $RESOLVE_CONF_FILE
+[Resolve]
+DNS=10.37.127.1
+Domains=~.
+EOF
+scp $RESOLVE_CONF_FILE devstack.lxd:/tmp/dns.conf
+ssh devstack.lxd "sudo cp /tmp/dns.conf /etc/systemd/resolved.conf; \
+    sudo systemctl restart systemd-resolved.service"
